09e11aafb4eab6556c1bdac19ddff1bcd52e4054
chandrakant sir <asterisctemp@gmail.com>
2022-07-09 10:15:37 +0530

    second commit i add program java

--- Test.java
+++ Test.java
@@ -1,7 +1,9 @@
 class Test
 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int a=20,b=55,c;
+        c=a+b;
+        System.out.println("Result "+c);
     }
 
 }
